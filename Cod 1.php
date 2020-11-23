65a8da67f0131cc3314c25e06a71914bfcc0fd3a
Jesus-Justiniano-Moya <73489934+Jesus-Justiniano-Moya@users.noreply.github.com>
2020-11-23 09:45:28 -0500

    Add files via upload

---
+++ Cod 1.php
@@ -0,0 +1,17 @@
+<? php
+/ * N1 Escriba un programa que muestre 
+el primer elemento del A-RRAY mostrado. 
+El resultado esperado es: blanco * /
+
+$ color = array ( 4 => 'blanco', 6 => 'verde', 11 => 'roja');
+echo  "<br>" ; echo  "<br>" ;
+$ color = array ( 4 => 'blanco', 6 => 'verde', 11 => 'roja');
+foreach ( $ color  como  $ n => $ colores ){
+  echo  $ n . "->" . $ colores . "<br>" ;
+                                           }
+echo  "<br>" ; echo  "<br>" ;
+$ color = array ( 4 => 'blanco' , 6 => 'verde' , 11 => 'roja' );
+para ( $ i = 0 ; $ i < 1 ; $ i ++){
+    echo  $ color [ 4 ];
+                                  }
+?>
